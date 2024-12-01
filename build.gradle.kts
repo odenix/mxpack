0a542f186228195cf576916acdc699a58ca95425
odenix <self@odenix.org>
2024-11-30 21:30:51 -0800

    Improve release build [skip ci]

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,13 @@ plugins {
   base
 }
 
-val centralBundle by tasks.registering(Zip::class) {
+val buildRelease by tasks.registering(Zip::class) {
   dependsOn("mxpack-core:publishMavenPublicationToFileRepository")
   dependsOn("mxpack-kotlin:publishMavenPublicationToFileRepository")
+  dependsOn("mxpack-core:check")
+  dependsOn("mxpack-kotlin:check")
   from(file("mxpack-core/build/m2"))
   from(file("mxpack-kotlin/build/m2"))
-  archiveFileName = "centralBundle.zip"
+  archiveFileName = "buildRelease.zip"
   destinationDirectory = layout.buildDirectory
 }
